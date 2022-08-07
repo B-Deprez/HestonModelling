9c23382a0a81a2357af486f7e37a47e31c386b3c
B-Deprez <bruno.r.deprez@gmail.com>
2022-08-07 19:35:20 +0200

    Implement optimisation with constraints

--- Calibration.R
+++ Calibration.R
@@ -1,4 +1,5 @@
 library(readxl)
+library(nloptr)
 
 dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(dir)
@@ -33,3 +34,28 @@ P_C_flag= data[,5][[1]]
 price = data[,6][[1]]
 int = r(Tt)
 
+sumofsquares <- function(p){
+  SoS = 0 
+  for(t in 1:length(Tt)){
+    SoS = SoS + ( HestonPricer(S0,Tt[t], q, int[t], p[4], K[t], p[1], p[2], p[3], p[5], P_C_flag[t]) - price[t] )^2
+  }
+  return(SoS/length(Tt))
+}
+
+# Constraint 
+mycon <- function(p){
+  return(p[3]^2-2*p[1]*p[2]+0*p[4]+0*p[5])
+}
+
+opts <- list( "algorithm"= "NLOPT_LN_COBYLA",
+              "xtol_rel"= 1.0e-15,
+              "maxeval"= 1000)
+
+res_H <- nloptr(
+  x0 = c(0.5, 0.05, 0.2, 0.05, -0.75),
+  eval_f = sumofsquares, 
+  lb = c(0, 0, 0, 0, -1),
+  ub = c(Inf, Inf, Inf, Inf, 1),
+  eval_g_ineq = mycon,
+  opts = opts
+); res_H
